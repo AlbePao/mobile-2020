37aeddcddc18363cad0b229f7a77a6f673298668
Alberto Paolucci <alberto.paolucci@icloud.com>
2021-04-02 13:09:07 +0200

    Update and format basic styles

--- theme/basic.js
+++ theme/basic.js
@@ -1,30 +1,45 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
     paddingLeft: 10,
-    paddingRight: 10
+    paddingRight: 10,
   },
   h1: {
     fontSize: 24,
-    fontWeight: "bold",
-    marginBottom: 12
+    fontWeight: 'bold',
+    marginBottom: 12,
   },
   paragraph: {
     fontSize: 18,
-    marginBottom: 8
+    marginBottom: 8,
+  },
+  flexRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+  },
+  flexStart: {
+    alignContent: 'flex-start',
+  },
+  flexEnd: {
+    alignContent: 'flex-end',
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
   },
   debug: {
-    backgroundColor: "black",
-    color: "green",
-    width: "100%",
+    backgroundColor: 'black',
+    color: 'green',
+    width: '100%',
     padding: 2,
     marginBottom: 1,
-    fontFamily: "Menlo" // only iOS
-  }
+    fontFamily: 'Menlo', // only iOS
+  },
 });
-
