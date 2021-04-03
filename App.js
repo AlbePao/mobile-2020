46a16524e5352ef48de0f211c456084867a3b3d3
Alberto Paolucci <alberto.paolucci@icloud.com>
2021-04-03 16:01:27 +0200

    App component with react native navigation

--- App.js
+++ App.js
@@ -1,41 +1,18 @@
-import { StatusBar } from "expo-status-bar";
-import React, { useState, useEffect } from "react";
-import { Text, View } from "react-native";
-import { fetchUsersPage } from "./services/users";
-import { basic } from "./theme";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import React from 'react';
+import { Home } from './views/Home';
+import { UserDetail } from './views/UserDetail';
 
-export default function App () {
+const RootStack = createStackNavigator();
+
+export default function App() {
   return (
-    <View style={basic.container}>
-      <StatusBar style="auto"/>
-
-      <Text style={basic.h1}>
-        Ready for a quick challenge?
-      </Text>
-      <Text style={basic.paragraph}>
-        Use services/fetchUsersPage for fetching the details of our users and their payment tables.
-      </Text>
-      <Text style={basic.paragraph}>
-        Display those pieces of information in a table.
-      </Text>
-
-      <Users/>
-    </View>
+    <NavigationContainer>
+      <RootStack.Navigator>
+        <RootStack.Screen name='Home' component={Home} />
+        <RootStack.Screen name='UserDetail' component={UserDetail} />
+      </RootStack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const Users = () => {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    async function fetchData () {
-      const usersList = await fetchUsersPage();
-      const data = await usersList.json();
-
-      setData([data.Users[0]]);
-    }
-
-    fetchData();
-  }, []);
-  return data.map(user => <Text style={basic.debug}>{user.email}</Text>);
-};
